2e97aee0de755a9abdfbc19f804f97a40915c73d
Wisselslaurens1 <wisselslaurens2000@gmail.com>
2021-03-29 11:31:29 +0200

    adding speler

--- domain/Speler.java
+++ domain/Speler.java
@@ -1,4 +1,31 @@
 package domain;
 
 public class Speler {
+
+    private String naam;
+    private int score;
+
+    public Speler(String naam) {
+        this.naam = naam;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void addToScore(int _score){
+        this.score += _score;
+    }
+
+    public boolean equals(Speler _speler){
+        if(this.naam.equals(_speler.naam) && this.score == _speler.score) return true;
+        return false;
+    }
+
+
 }

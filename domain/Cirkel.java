6e3dff608fad46151a4f741371f970e0c4d51932
HisashiSenpai <kaczynskiadam96@gmail.com>
2021-03-29 13:43:24 +0200

    Enkele methodes toegevoegd aan klasse Cirkel

--- domain/Cirkel.java
+++ domain/Cirkel.java
@@ -1,4 +1,24 @@
 package domain;
 
 public class Cirkel {
+    private Punt middelPunt;
+    private int radius;
+        public Cirkel(Punt punt, int radius){
+            this.middelPunt = punt;
+            this.radius = radius;
+
+        }
+    private void setMiddelPunt(Punt punt){
+            this.middelPunt = punt;
+        }
+        private void setRadius(int radius){
+            this.radius = radius;
+        }
+        public Punt getMiddelPunt(){
+            return middelPunt;
+        }
+        public int getRadius(){
+            return radius;
+        }
+
 }

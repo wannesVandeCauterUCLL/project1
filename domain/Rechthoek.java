8ddedc46907ca7c6ac05a03da9eff3bdbeaa42dc
wannesVandeCauterUCLL <wannes.vandecauter@student.ucll.be>
2021-03-29 15:00:30 +0200

    laatste merge fout Rechthoek.java

--- domain/Rechthoek.java
+++ domain/Rechthoek.java
@@ -10,7 +10,9 @@ public class Rechthoek {
         if (breedte <= 0 || hoogte <= 0){
             throw new DomainException("Rechthoek heeft een breedte en hoogte groter dan 0 nodig.");
         }
-
+        if (this.linkerBovenHoek == null){
+            throw new DomainException("Rechthoek heeft een linkerbovenhoek nodig.");
+        }
         this.breedte = breedte;
         this.hoogte = hoogte;
         this.linkerBovenHoek = linkerBovenhoek;

eec32369affa53a81b7b5716213d5c9da4c9f1a4
Wisselslaurens1 <wisselslaurens2000@gmail.com>
2021-03-29 14:42:57 +0200

    adding punt

--- domain/Punt.java
+++ domain/Punt.java
@@ -1,22 +1,35 @@
 package domain;
 
 public class Punt {
-    public Punt(int parseInt, int parseInt1) {
-
-    }
 
     private int x;
     private int y;
 
+    public Punt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+
     public int getY() {
-        return x;
+        return this.y;
     }
 
     public int getX() {
-        return x;
+        return this.x;
     }
 
     public void setX(int x) {
         this.x = x;
+    }
+
+    public boolean equals(Punt _punt){
+        if(this.x == _punt.x && this.y == _punt.y) return true;
+        return false;
+    }
+
+    public String toString(){
+        return "X: "+this.x +" Y: "+this.y;
     }
 }
---
+++ domain/Rechthoek.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Rechthoek {
+
+    private Punt linkerBovenhoek;
+
+    public Rechthoek(Punt linkerBovenhoek, int breedte, int hoogte) {
+    }
+
+    public Punt getLinkerBovenhoek() {
+        return this.linkerBovenhoek;
+    }
+
+    public int getHoogte() {
+        return 0;
+    }
+
+    public int getBreedte() {
+        return 0;
+    }
+}

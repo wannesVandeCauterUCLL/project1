ec88c932b6b23db17bd65774d1af716d06cfd9d8
wannesVandeCauterUCLL <wannes.vandecauter@student.ucll.be>
2021-03-31 11:45:26 +0200

    omhullende bug-fix

--- domain/OmhullendeFactory.java
+++ domain/OmhullendeFactory.java
@@ -10,7 +10,7 @@ public class OmhullendeFactory {
         /****** Rechthoek *******/
         if(vorm instanceof Rechthoek){
             Rechthoek rechthoek = ((Rechthoek) vorm);
-            return new Omhullende(rechthoek.getLinkerBovenhoek(),rechthoek.getBreedte(),rechthoek.getHoogte());
+            return new Omhullende(rechthoek.getLinkerBovenhoek(),Math.abs(rechthoek.getBreedte()),Math.abs(rechthoek.getHoogte()));
 
             /******* Cirkel *******/
         }else if(vorm instanceof Cirkel){
@@ -29,7 +29,7 @@ public class OmhullendeFactory {
             int breedte = hoekpunten.get(2).getX()-hoekpunten.get(0).getX();
             Collections.sort(hoekpunten, Punt.yComparator);
             int hoogte = hoekpunten.get(2).getY()-hoekpunten.get(0).getY();
-            return new Omhullende(linkerBovenHoek,breedte,hoogte);
+            return new Omhullende(linkerBovenHoek,Math.abs(breedte),Math.abs(hoogte));
 
             /******* Lijnstuk *******/
         }else if(vorm instanceof LijnStuk){
@@ -37,7 +37,7 @@ public class OmhullendeFactory {
             Punt linkerBovenHoek = new Punt(lijn.getStartPunt().getX(),lijn.getStartPunt().getY());
             int breedte = lijn.getEindPunt().getX() - lijn.getStartPunt().getX();
             int hoogte = lijn.getEindPunt().getY() - lijn.getStartPunt().getY();
-            return new Omhullende(linkerBovenHoek, breedte,hoogte);
+            return new Omhullende(linkerBovenHoek, Math.abs(breedte),Math.abs(hoogte));
         }else{
             return null;
         }

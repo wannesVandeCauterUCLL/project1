e1c09f8ab7e06a5d817bc973538bbe422d8e2f17
HisashiSenpai <kaczynskiadam96@gmail.com>
2021-03-31 09:31:22 +0200

    Vorm methodes aangepast

--- domain/Vorm.java
+++ domain/Vorm.java
@@ -1,7 +1,24 @@
 package domain;
 
+import javafx.scene.paint.Color;
+
 public abstract class Vorm {
+    private Color kleur;
 
+    public Color getKleur(){
+        return kleur;
+    }
 
-
+    public void setKleur(Color kleur){
+        this.kleur = kleur;
+    }
+    /**
+     * Van waar komt die omhullende hier?
+     */
+    //public Omhullende getOmhullende(){
+    //
+    //}
+    public String toString(){
+        return "De kleur van deze vorm: "+this.kleur+". Omhullende:";//+this.getOmhullende();
+    }
 }

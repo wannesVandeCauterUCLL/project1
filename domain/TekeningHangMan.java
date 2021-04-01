28e9732f82ba8dea651c77c4adb186a29bb508f9
wannesVandeCauterUCLL <wannes.vandecauter@student.ucll.be>
2021-04-01 10:42:06 +0200

    story 14 af

--- domain/TekeningHangMan.java
+++ domain/TekeningHangMan.java
@@ -6,15 +6,13 @@ import java.util.ArrayList;
 
 public class TekeningHangMan extends Tekening{
 
-    //private ArrayList<Vorm> vormenHangMan;
+    private int indexEersteOnzichtbare = 4;
 
     public TekeningHangMan(){
         super("Hangman");
-        //this.vormenHangMan = new ArrayList<Vorm>();
         try {
             this.voegVormenToe();
         }catch (DomainException e){
-            System.out.println(e);
         }
 
 
@@ -29,10 +27,15 @@ public class TekeningHangMan extends Tekening{
         Vorm oogLinks = new Cirkel(new Punt(270, 118), 2);
         Vorm oogRechts = new Cirkel(new Punt(290, 118), 2);
         Vorm neus = new Cirkel(new Punt(280, 128), 2);
-        Vorm mond = new LijnStuk(new Punt(270, 138), new Punt(290, 138)); Vorm lijf = new LijnStuk(new Punt(280, 150), new Punt(280, 250));
-        Vorm beenLinks = new LijnStuk(new Punt(280, 250), new Punt(240, 310)); Vorm beenRechts = new LijnStuk(new Punt(280, 250), new Punt(320, 310)); Vorm voetLinks = new Cirkel(new Punt(240, 310), 5);
+        Vorm mond = new LijnStuk(new Punt(270, 138), new Punt(290, 138));
+        Vorm lijf = new LijnStuk(new Punt(280, 150), new Punt(280, 250));
+        Vorm beenLinks = new LijnStuk(new Punt(280, 250), new Punt(240, 310));
+        Vorm beenRechts = new LijnStuk(new Punt(280, 250), new Punt(320, 310));
+        Vorm voetLinks = new Cirkel(new Punt(240, 310), 5);
         Vorm voetRechts = new Cirkel(new Punt(320, 310), 5);
-        Vorm armLinks = new LijnStuk(new Punt(280, 200), new Punt(230, 170)); Vorm armRechts = new LijnStuk(new Punt(280, 200), new Punt(330, 170)); Vorm handLinks = new Cirkel(new Punt(230, 170), 5);
+        Vorm armLinks = new LijnStuk(new Punt(280, 200), new Punt(230, 170));
+        Vorm armRechts = new LijnStuk(new Punt(280, 200), new Punt(330, 170));
+        Vorm handLinks = new Cirkel(new Punt(230, 170), 5);
         Vorm handRechts = new Cirkel(new Punt(330, 170), 5);
 
         hoofd.setZichtbaar(false);
@@ -40,23 +43,36 @@ public class TekeningHangMan extends Tekening{
         oogRechts.setZichtbaar(false);
         neus.setZichtbaar(false);
         mond.setZichtbaar(false);
+
+        lijf.setZichtbaar(false);
         beenLinks.setZichtbaar(false);
+        beenRechts.setZichtbaar(false);
+        voetLinks.setZichtbaar(false);
         voetRechts.setZichtbaar(false);
         armLinks.setZichtbaar(false);
+        armRechts.setZichtbaar(false);
+        handLinks.setZichtbaar(false);
         handRechts.setZichtbaar(false);
 
         this.vormen.add(galgBodem);
         this.vormen.add(galgStaaf);
         this.vormen.add(hangbar);
         this.vormen.add(koord);
+
         this.vormen.add(hoofd);
         this.vormen.add(oogLinks);
         this.vormen.add(oogRechts);
         this.vormen.add(neus);
         this.vormen.add(mond);
+
+        this.vormen.add(lijf);
         this.vormen.add(beenLinks);
+        this.vormen.add(beenRechts);
+        this.vormen.add(voetLinks);
         this.vormen.add(voetRechts);
         this.vormen.add(armLinks);
+        this.vormen.add(armRechts);
+        this.vormen.add(handLinks);
         this.vormen.add(handRechts);
     }
 
@@ -72,19 +88,14 @@ public class TekeningHangMan extends Tekening{
     }
 
     public void zetVolgendeZichtbaar() throws DomainException {
-        boolean currentZichtbaar = true;
-        int i = -1;
-        Vorm vorm = null;
-        while (currentZichtbaar && i+1<this.getAantalVormen()){
-            i++;
-            vorm = this.getVorm(i);
-            currentZichtbaar = vorm.isZichtbaar();
+        if(this.getAantalOnzichtbaar() > 0){
+            Vorm vorm = this.getVorm(this.indexEersteOnzichtbare);
+            vorm.setZichtbaar(true);
+            this.indexEersteOnzichtbare++;
         }
-        if (currentZichtbaar){
+        else {
             throw new DomainException("Alle vormen zijn al zichtbaar");
         }
-        //i geeft index van eerste onzichbare element
-        vorm.setZichtbaar(true);
     }
 
     @Override

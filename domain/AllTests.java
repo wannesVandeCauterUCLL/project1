13c821274b442d33ba613239b5b39bb1b437b6f0
wannesVandeCauterUCLL <wannes.vandecauter@student.ucll.be>
2021-04-01 14:26:18 +0200

    AllTests gefixt

--- domain/AllTests.java
+++ domain/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class})
+@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, DriehoekTest.class, HangManTest.class, HintWoordTest.class, HintLetterTest.class, LijnStukTest.class, OmhullendeTest.class, RechthoekTest.class, TekeningHangManTest.class, TekeningTest.class, WoordenLijstTest.class})
 public class AllTests {
 
 }
